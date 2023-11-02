0d3d8cf99bf5a3ad7e6a054e82f4ee88fab758f4
Шульгин Григорий <shulgin.grigoriy@yandex.ru>
2023-11-02 17:16:16 +0300

    add default type input to new-password

--- src/InputWithHints.vue
+++ src/InputWithHints.vue
@@ -76,7 +76,7 @@ export default defineComponent({
     },
     htmlInputType: {
       type: String,
-      default: 'text',
+      default: 'new-password',
     },
     disabled:{
       type:Boolean,
